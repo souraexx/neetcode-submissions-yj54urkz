ea6b0a2698435b5350cb2e25a6a8be805d97056f
Souradeep Dutta <sourae.47@gmail.com>
2026-04-01 23:43:12 +0530

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        Arrays.fill(dp, -1);
+        return getClimbStairs(n, dp);
+    }
+    int getClimbStairs(int index, int dp[]) {
+        if (index==0) return 1;
+        if (index==1) return 1;
+        if (dp[index] != -1) return dp[index];
+        int left = getClimbStairs(index-1, dp);
+        int right = getClimbStairs(index-2, dp);
+        return dp[index] = left+right;
+    }
+}
