0cad5bf9acd96de036f86337d8393ad7f126c79c
Souradeep Dutta <sourae.47@gmail.com>
2026-04-03 02:41:20 +0530

    Add: n-th-tribonacci-number - submission-1

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n==0) return 0;
+        if (n==1) return 1;
+        if (n==2) return 1;
+        int t0 = 0;
+        int t1 = 1;
+        int t2 = 1;
+        for (int i=3;i<=n;i++) {
+            int tn = t0+t1+t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }    
+        return t2;
+    }
+}
