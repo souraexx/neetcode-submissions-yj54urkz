74ef541bee4cbfa72ffa18eca1505615d8cf7d88
Souradeep Dutta <sourae.47@gmail.com>
2026-04-15 15:04:07 +0530

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int result = findCoins(n-1, coins, amount);
+        if (result!=(int)1e9) {
+            return result;
+        } else {
+            return -1;
+        }
+    }
+    int findCoins(int index, int[] coins, int amount) {
+        if (index == 0) {
+            if (amount==0 && coins[0]!=0) return 0;
+            if (amount==0) return 1;
+            if (amount%coins[index] == 0) return amount/coins[index];
+            else return (int)1e9;
+        }
+
+        int notPick = findCoins(index-1, coins, amount);
+        int pick = (int)1e9;
+        if (coins[index]<=amount) {
+            pick =  1 + findCoins(index, coins, amount-coins[index]);
+        }
+
+        return Math.min(pick, notPick);
+    }
+}
